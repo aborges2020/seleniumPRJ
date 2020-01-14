5172fec58c7778fbdd3dc9c5a14e0558ba419bcd
aborges-srv-01 <aborges2020@gmail.com>
2020-01-13 23:38:57 -0500

    first version.

--- src/main/java/session_2/jenkins_demo.java
+++ src/main/java/session_2/jenkins_demo.java
@@ -11,16 +11,16 @@ public class jenkins_demo
     void publicvoidtestgooglrsearch(){
 
         WebDriver driver = new FirefoxDriver();
-//it will open the goggle page
+
         driver.get("http://google.in");
-//we expect the title “Google “ should be present
+
         String Expectedtitle = "Google";
-//it will fetch the actual title
+
         String Actualtitle = driver.getTitle();
         System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
-//it will compare actual title and expected title
+
         Assert.assertEquals(Actualtitle, Expectedtitle);
-//print out the result
+
         System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
     }
 
